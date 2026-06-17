25c463be5cbb88cf989dd096d4dc1ee35d5ea632
DhamodharanJ <dhamodharanj8@gmail.com>
2026-06-17 18:19:37 +0530

    Updated Batch Repository

--- src/main/java/com/example/Interview_Scheduler/repository/BatchRepository.java
+++ src/main/java/com/example/Interview_Scheduler/repository/BatchRepository.java
@@ -4,7 +4,6 @@ import com.example.Interview_Scheduler.model.BatchModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-
 @Repository
 public interface BatchRepository extends JpaRepository<BatchModel, Long> {
 
