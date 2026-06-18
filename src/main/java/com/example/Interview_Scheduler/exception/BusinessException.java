2eedc1c2a63e09622102ab0efb5423c389b3425e
monikasrim <monikasrim05@gmail.com>
2026-06-18 16:52:04 +0530

    created BusinessException

---
+++ src/main/java/com/example/Interview_Scheduler/exception/BusinessException.java
@@ -0,0 +1,7 @@
+package com.example.Interview_Scheduler.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException (String message) {
+        super(message);
+    }
+}
